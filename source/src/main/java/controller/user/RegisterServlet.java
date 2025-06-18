f10277dc4364b05fd50a04427908387e8c6f5b10
plusdojo2025-Nakagawa <nakagawa-aika-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-18 10:48:12 +0900

    RegisterServlet作成

---
+++ source/src/main/java/controller/user/RegisterServlet.java
@@ -0,0 +1,51 @@
+package controller.user;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.dto.UserDto;
+import model.service.UserService;
+import util.Validator;
+
+public class RegisterServlet extends HttpServlet {
+	
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException,IOException {
+		
+		String username = request.getParameter("username");
+		String email = request.getParameter("email");
+		String password = request.getParameter("password");
+		
+		//入力チェック
+		if (Validator.isEmpty(username) ||  
+			!Validator.isWithinLength(username, 1, 100) ||
+			!Validator.isEmail(email) ||
+			!Validator.isPasswordComplex(password)) {
+			
+			request.setAttribute("errorMessage", "入力内容に不備があります。再度ご確認ください。");
+			request.getRequestDispatcher("/WEB-INF/jsp/user/register.jsp").forward(request,response);
+			return;
+		}
+		
+		//Dto作成（パスワードはValidatorのハッシュ関数で暗号化）
+		UserDto dto = new UserDto();
+		dto.setUsername(username);
+		dto.setEmail(email);
+		dto.setPasswordHash(Validator.hashPassword(password));
+		
+		try {
+			UserService service = new UserService();
+			service.register(dto);
+			response.sendRedirect("login");
+		} catch (Exception e) {
+			request.setAttribute("errorMessage","登録処理中にエラーが発生しました。"+ e.getMessage());
+			request.getRequestDispatcher("/WEB-INF/jsp/user/register.jsp").forward(request, response);
+		}
+	}
+	
+}
